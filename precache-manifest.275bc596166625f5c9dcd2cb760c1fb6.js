0971d42d7f7f8c17e8a117a6be9fd132da7dcb28
ErnestoSalazar <ernestoalbertosalazar@gmail.com>
2019-04-18 10:15:16 -0700

    Updates

---
+++ precache-manifest.275bc596166625f5c9dcd2cb760c1fb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "1bbda802fbf515d40064",
+    "url": "/course-directory/static/js/main.1bbda802.chunk.js"
+  },
+  {
+    "revision": "f5ae2494a8dbd9f95f4b",
+    "url": "/course-directory/static/js/1.f5ae2494.chunk.js"
+  },
+  {
+    "revision": "1bbda802fbf515d40064",
+    "url": "/course-directory/static/css/main.4eb1514b.chunk.css"
+  },
+  {
+    "revision": "29cf9a962e6ba79644768af8ffad4330",
+    "url": "/course-directory/index.html"
+  }
+];
